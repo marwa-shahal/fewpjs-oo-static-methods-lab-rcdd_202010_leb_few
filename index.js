1d96547c6400fc664d2978488bf8aa47c0d779ff
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-19 09:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9' -]+/g, '');
     }
     
-    static titleize(string){
-      let exceptions=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
-      let word=string.split(" ")
-      let result=word.map( (w,index) => {
-        if (index===0){
-          return w.charAt(0).toUpperCase() + string.slice(1);
-        }else if(exceptions.findIndex(n=>n===w)!==-1){
-          return w;
-        }else{
-          return w.charAt(0).toUpperCase() + string.slice(1);
-        }
-       });
-     
-      return result.join(" ");
+
+function titleize(string) {
+  let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  let word = string.split(" ")
+  let result = word.map((w, index) => {
+    if (index === 0) {
+      return w.charAt(0).toUpperCase() + w.slice(1);
     }
- 
+    if (exceptions.findIndex(n => n === w) !== -1) {
+      return w;
+    }
+    return w.charAt(0).toUpperCase() + w.slice(1);
+  });
+
+  return result.join(" ");
+}
+
 }
